122beea5329b2a60b2a0645e404f10707b586f85
Mitchell Hudson <soggybag@gmail.com>
2015-11-22 10:29:03 -0800

    Initial Commit

---
+++ TableView-Custom-Cell-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TableView-Custom-Cell-Test
+//
+//  Created by mitchell hudson on 11/22/15.
+//  Copyright © 2015 mitchell hudson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
